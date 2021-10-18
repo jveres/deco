26ab6eaf2fbee90007333889d221e2ab35d3a68b
Janos Veres <janos.veres@gmail.com>
2021-10-18 08:37:59 +0200

    updates

--- examples/dapr/example_dapr.ts
+++ examples/dapr/example_dapr.ts
@@ -2,10 +2,10 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
-// Run the example:
-//    dapr --app-id deco-app --app-port 3000 --components-path ./components run -- deno run -A --unstable --watch example_dapr.ts
 // Start Dapr sidecar in local environment:
 //    dapr run --app-id sidecar --dapr-http-port 3500 --components-path ./components
+// Run the example:
+//    dapr --app-id deco-app --app-port 3000 --components-path ./components run -- deno run -A --unstable --watch example_dapr.ts
 // Publish message to topic A:
 //    dapr publish --publish-app-id sidecar --pubsub pubsub --topic A --data '{"data": "message for topic A"}'
 // Publish message to topic B and get Telegrom notification (needs TELEGRAM_TOKEN and TELEGRAM_CHATID exist in the secrets store):
