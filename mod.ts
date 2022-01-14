a9592cb27517e79529cee565b5f2682cb250ab98
jveres <janos.veres@gmail.com>
2022-01-14 10:21:55 +0100

    export more decorators

--- mod.ts
+++ mod.ts
@@ -2,6 +2,11 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
+export * from "./decorators/cache.decorator.ts";
+export * from "./decorators/concurrency.decorator.ts";
 export * from "./decorators/httpserver.decorator.ts";
+export * from "./decorators/ratelimit.decorator.ts";
+export * from "./decorators/timeout.decorator.ts";
+export * from "./decorators/trace.decorator.ts";
 export * from "./utils/utils.ts";
 export * from "./version.ts";
--- version.ts
+++ version.ts
@@ -1,1 +1,1 @@
-export const DECO_VERSION = "0.9.0";
+export const DECO_VERSION = "0.9.1";
